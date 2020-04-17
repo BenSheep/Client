a16c945c9d0618c078ce97ff4b9efdbf22c37c12
DimosPagakis <dimosthenispagakis@gmail.com>
2020-04-17 19:58:19 +0300

    :tada: Initial commit with sample routing

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import DefaultLayout from "../components/DefaultLayout";
+
+export default function About() {
+  return (
+    <DefaultLayout>
+      <p>This is the about page</p>
+    </DefaultLayout>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import DefaultLayout from "../components/DefaultLayout";
+export default function Index() {
+  return (
+    <DefaultLayout>
+      <p>Hello Next.js</p>
+    </DefaultLayout>
+  );
+}
