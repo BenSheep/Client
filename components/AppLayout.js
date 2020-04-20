44db9331eec1d0aa61039c8183c1a94feb133c7f
DimosPagakis <dimosthenispagakis@gmail.com>
2020-04-20 20:42:38 +0300

    :lipstick: Adding left out shared layout

---
+++ components/AppLayout.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'next/link'
+
+const AppLayout = props => (
+  <div className="w-full mx-auto flex flex-wrap flex-col lg:flex-row relative">
+    <div className="flex-col w-full lg:w-1/12 border px-4">
+      <nav data-test="navbar">
+        <ul>
+          <li>
+            <Link href="/app/courses">
+              <a data-test="courses-tab-button">My courses</a>
+            </Link>
+          </li>
+          <li>
+            <a>Nani</a>
+          </li>
+          <li>
+            <a>Nani</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div className="flex-col w-11/12 lg:px-12">{props.children}</div>
+  </div>
+)
+
+export default AppLayout
