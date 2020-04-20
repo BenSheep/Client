83d7b034bf63f66dd8647b613cddd71028c6ebae
DimosPagakis <dimosthenispagakis@gmail.com>
2020-04-20 18:43:01 +0300

    :white_check_mark: Test for get courses action

---
+++ store/actions/coursesActions.js
@@ -0,0 +1,31 @@
+import api from '../api'
+
+export const STORE_COURSES = 'STORE_COURSES'
+
+const query = `query{
+    myCourses {
+        name,
+        schedule {
+            day
+        }
+    }
+}`
+
+export const getCourses = token => dispatch => {
+  const options = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+  return api.post('', { query }, options).then(res => {
+    if (!res.data.errors) {
+      const courses = res.data.data.myCourses
+      console.log(courses)
+
+      dispatch({
+        type: STORE_COURSES,
+        courses,
+      })
+    }
+  })
+}
---
+++ tests/store/coursesAction.spec.js
@@ -0,0 +1,42 @@
+import { getCourses, STORE_COURSES } from '~/store/actions/coursesActions'
+import api from '~/store/api'
+jest.mock('../../store/api')
+
+describe('Courses actions', () => {
+  describe('Get courses', () => {
+    it('stores courses retrieved from the API', () => {
+      const courses = [
+        {
+          name: 'French  translation',
+          schedule: [
+            {
+              day: 3,
+            },
+          ],
+        },
+        {
+          name: 'Networks',
+          schedule: [],
+        },
+      ]
+
+      const dispatch = jest.fn()
+
+      api.post.mockResolvedValue({
+        data: {
+          errors: null,
+          data: {
+            myCourses: courses,
+          },
+        },
+      })
+
+      return getCourses()(dispatch).then(() => {
+        expect(dispatch).toHaveBeenCalledWith({
+          type: STORE_COURSES,
+          courses,
+        })
+      })
+    })
+  })
+})
