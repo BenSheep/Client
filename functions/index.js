d1684e67bca147e387bfd45ffe9d42831ddfb68b
DimosPagakis <dimosthenispagakis@gmail.com>
2020-04-23 22:15:03 +0300

    :lipstick: Updated course details page UI

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,17 @@
 export const capitalize = name => {
   return name.charAt(0).toUpperCase() + name.substring(1, name.length)
 }
+
+export const formatNumberTh = number => {
+  const lastDigit = number.toString().charAt(0)
+  switch (lastDigit) {
+    case '1':
+      return `${number}st`
+    case '2':
+      return `${number}nd`
+    case '3':
+      return `${number}rd`
+    default:
+      return `${number}th`
+  }
+}
--- pages/app/courses/[name].js
+++ pages/app/courses/[name].js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 
 import AppLayout from '~/components/AppLayout'
 import { getCourseByName } from '~/store/actions/coursesActions'
-import { capitalize } from '~/functions'
+import { capitalize, formatNumberTh } from '~/functions'
 
 class CourseDetailsPage extends Component {
   static getInitialProps({ query }) {
@@ -24,15 +24,32 @@ class CourseDetailsPage extends Component {
     return (
       <AppLayout>
         {courseDetails ? (
-          <div data-test="course-detail-card" className="text-blue">
-            <h1 className="text-xl md:text-4xl" data-test="course-name">
-              {capitalize(courseDetails.name)}
+          <div
+            data-test="course-detail-card"
+            className="flex flex-wrap flex-col md:flex-row w-4/5 mx-auto text-blue bg-silver rounded-lg p-4 mt-8"
+          >
+            <h1
+              className="w-full flex text-xl md:text-4xl mb-8"
+              data-test="course-name"
+            >
+              {capitalize(courseDetails.name)} -{' '}
+              {formatNumberTh(courseDetails.semester)} semester
             </h1>
-            <p>Semester: {courseDetails.semester}</p>
             {courseDetails.grade ? (
-              <h3 data-test="course-grade">
-                Your grade is {courseDetails.grade}
-              </h3>
+              <div className="flex flex-wrap flex-col w-full mx-auto">
+                <h3
+                  data-test="course-grade-header"
+                  className="flex mx-auto text-xl md:text-4xl text-blue font-semibold"
+                >
+                  Grade
+                </h3>
+                <p
+                  data-test="course-grade"
+                  className="flex mx-auto text-xl md:text-4xl text-blue font-semibold"
+                >
+                  {courseDetails.grade}
+                </p>
+              </div>
             ) : null}
           </div>
         ) : (
